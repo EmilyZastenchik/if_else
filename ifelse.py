e368fbf5e11ecf1b5fae7144e8d8811165192018
EmilyZastenchik <43222216+EmilyZastenchik@users.noreply.github.com>
2019-06-24 13:29:30 -0600

    upload

---
+++ ifelse.py
@@ -0,0 +1,34 @@
+#temp = float (input ('What is the temp?'))
+#if temp > 70:
+ #   print("wear shorts")
+#else:
+ #   print("wear long pants")
+#print("get some exercise outside")
+
+
+
+#grade = int(input("Enter grade: "))
+#if grade >=90:
+ #   print("Congratulations")
+#else:
+ #   print("Beter luck next time")
+
+
+grade = int(input("Enter grade: "))
+
+if(grade in range(90,100)):
+     print("A")
+elif(grade in range(80,89)):
+     print("B")
+elif(grade in range(70,79)):
+     print("C")
+elif(grade in range(60,69)):
+     print("D")
+elif(grade<60):
+     print("F")
+
+    
+
+    
+    
+
